2e0152085b1acef9e68f90fb1ee5389194f610ba
woeph7 <aeishin0212@gmail.com>
2020-01-18 10:51:12 +0100

    fin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types'
 import './App.css';
 
-function App() {
+function VideoThumbnail({ title, preview, badge, badgeBG, message, muted, width, classname }) {
+  const ref = React.useRef(null)
+
+  const play = () => {
+    ref.current.play()
+    ref.current.nextElementSibling.style.opacity = .4
+  }
+  const pause = () => {
+    ref.current.pause()
+    ref.current.nextElementSibling.style.opacity = 1
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div style={{ width: width }} className={`${classname}`}>
+      <div style={{ width: width }} className="video">
+        <video
+          onMouseLeave={() => pause()}
+          onMouseEnter={() => play()}
+          ref={ref} src={preview}
+          loop
+          width={width}
+          muted={muted} />
+        <div className="badge" style={{ background: badgeBG }}>{badge}</div>
+      </div >
+      <h4 className="title"> {title}</h4 >
+      <span className="views">{message}</span>
+    </div >
+  )
 }
 
-export default App;
+VideoThumbnail.prototype = {
+  preview: PropTypes.string,
+  title: PropTypes.string,
+  message: PropTypes.string,
+  badge: PropTypes.string,
+  badgeBg: PropTypes.string,
+  muted: PropTypes.bool,
+  width: PropTypes.number,
+  classname: PropTypes.string
+}
+
+VideoThumbnail.defaultProps = {
+  preview: `https://media2.giphy.com/media/JrY9JfnL4OnXxnCi4F/giphy480p.mp4`,
+  title: `The Invisible Man - Official Trailer [HD]`,
+  message: `3.2k Watching Now`,
+  badge: `Live`,
+  badgeBG: "blue",
+  muted: true,
+  width: 350,
+  classname: 'videoThumbnail'
+}
+
+export default VideoThumbnail;
